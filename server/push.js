35c43be0a741a85304c2eaf6c1bd90c5e4e72eaa
yvromero <yvonne.romero@uds.edu.py>
2023-07-07 19:13:27 -0400

    Set up web-push

--- server/push.js
+++ server/push.js
@@ -3,6 +3,14 @@ const fs = require('fs');
 const urlsafeBase64 = require('urlsafe-base64');
 const vapid = require('./vapid.json');
 
+const webpush = require('web-push');
+
+webpush.setVapidDetails(
+    'mailto:luntiromer@gmail.com',
+    vapid.publicKey,
+    vapid.privateKey
+);
+
 const suscripciones = [];
 
 
